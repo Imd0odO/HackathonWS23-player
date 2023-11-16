bd6f37d25f7b84d25e62b7b8ee8031ca57f6ea78
Timo Schneider <timoschneider04@gmail.com>
2023-11-17 00:05:16 +0100

    es ist Zeit für Zeit und Randomness pro Zeit, Runden und 42 ab Runde 3 in einer Zeile

--- logic/strategy.ts
+++ logic/strategy.ts
@@ -1,10 +1,7 @@
 import {Bet, Table} from "../models";
 
 export function decide(table: Table): Bet {
-//TODO: Add Poker Logic Here... :)
+    //TODO: Add Poker Logic Here... :)
 
-    if (((Math.random() * Date.now()) / (table.pot+table.round+42)) % 2) {
-        return {bet: table.players[table.activePlayer].stack}
-    }
-    return {bet: 0}
+    return (((Math.random() * Date.now()) / (table.pot+table.round+42)) % 2 && table.round > 2) ? {bet: table.players[table.activePlayer].stack} : {bet: 0}
 }
