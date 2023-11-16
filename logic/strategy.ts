bfd3b4413ae33db0ed9743556fa594d2e3dbaef4
Timo Schneider <timoschneider04@gmail.com>
2023-11-16 23:47:09 +0100

    es ist Zeit für Zeit

--- logic/strategy.ts
+++ logic/strategy.ts
@@ -3,7 +3,7 @@ import {Bet, Table} from "../models";
 export function decide(table: Table): Bet {
 //TODO: Add Poker Logic Here... :)
 
-    if (Date.now() % 2) {
+    if ((Date.now() / 1000) % 2) {
         return {bet: table.players[table.activePlayer].stack}
     }
     return {bet: 0}
