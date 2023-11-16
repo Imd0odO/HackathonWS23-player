4bfc83f2ab8764766d96e533b07db101ac8f0e6e
Timo Schneider <timoschneider04@gmail.com>
2023-11-16 23:45:04 +0100

    es ist Zeit zu schlafen

--- logic/strategy.ts
+++ logic/strategy.ts
@@ -3,14 +3,8 @@ import {Bet, Table} from "../models";
 export function decide(table: Table): Bet {
 //TODO: Add Poker Logic Here... :)
 
-    if (table.activePlayer && table.round > 3){
-        let max_ratio = 0
-        table.players.slice(table.activePlayer - 1).map(player => player.bet / player.stack).forEach(bet_ratio => max_ratio = bet_ratio > max_ratio? bet_ratio : max_ratio )
-
-        if (max_ratio < 0.6){
-            return {bet: table.players[table.activePlayer].stack}
-        }
-        return {bet: 42 * 0.69}
+    if (Date.now() % 2) {
+        return {bet: table.players[table.activePlayer].stack}
     }
     return {bet: 0}
 }
