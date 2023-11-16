a0c9c51e7d82f434d1c33f8bd78b6fe367071460
Timo Schneider <timoschneider04@gmail.com>
2023-11-16 23:57:43 +0100

    es ist Zeit für Zeit und Randomness, und Geld+42 in Runden

--- logic/strategy.ts
+++ logic/strategy.ts
@@ -3,7 +3,7 @@ import {Bet, Table} from "../models";
 export function decide(table: Table): Bet {
 //TODO: Add Poker Logic Here... :)
 
-    if ((Math.random() * Date.now() * table.round / (table.pot+1)) % 2) {
+    if ((Math.random() * Date.now() * table.round / (table.pot+42)) % 2) {
         return {bet: table.players[table.activePlayer].stack}
     }
     return {bet: 0}
