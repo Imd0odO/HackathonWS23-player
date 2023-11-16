450928c623bca463ff99cf9d50e04fe8e5ec51a7
Timo Schneider <timoschneider04@gmail.com>
2023-11-16 23:15:54 +0100

    zufall it is then

--- logic/strategy.ts
+++ logic/strategy.ts
@@ -3,8 +3,10 @@ import {Bet, Table} from "../models";
 export function decide(table: Table): Bet {
 //TODO: Add Poker Logic Here... :)
     let bet = 0
-    if (table.round > 20){
-        bet = table.players[table.activePlayer].stack
+    if (table.round > 5){
+        if(Math.random() > 0.75){
+            bet = table.players[table.activePlayer].stack
+        }
     }
 
     return {bet: bet}
