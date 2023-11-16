1bdbc7818923bf84ee7e93157ae320f3e2134f26
Timo Schneider <timoschneider04@gmail.com>
2023-11-16 23:18:45 +0100

    42

--- logic/strategy.ts
+++ logic/strategy.ts
@@ -4,7 +4,7 @@ export function decide(table: Table): Bet {
 //TODO: Add Poker Logic Here... :)
     let bet = 0
     if (table.round > 5){
-        if(Math.random() > 0.75){
+        if(Math.random() < 0.42){
             bet = table.players[table.activePlayer].stack
         }
     }
