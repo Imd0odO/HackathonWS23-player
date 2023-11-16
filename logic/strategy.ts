4a331e127a0c14a1b6bdaaa66b78b32b35ede169
Timo Schneider <timoschneider04@gmail.com>
2023-11-16 23:13:36 +0100

    back again v2

--- logic/strategy.ts
+++ logic/strategy.ts
@@ -3,7 +3,7 @@ import {Bet, Table} from "../models";
 export function decide(table: Table): Bet {
 //TODO: Add Poker Logic Here... :)
     let bet = 0
-    if (table.round > 10){
+    if (table.round > 20){
         bet = table.players[table.activePlayer].stack
     }
 
