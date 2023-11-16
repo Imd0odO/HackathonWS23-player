58733d8690dc0c9637598178883291288913482f
Timo Schneider <timoschneider04@gmail.com>
2023-11-16 23:20:45 +0100

    42 for the win

--- logic/strategy.ts
+++ logic/strategy.ts
@@ -2,12 +2,5 @@ import {Bet, Table} from "../models";
 
 export function decide(table: Table): Bet {
 //TODO: Add Poker Logic Here... :)
-    let bet = 0
-    if (table.round > 5){
-        if(Math.random() < 0.42){
-            bet = table.players[table.activePlayer].stack
-        }
-    }
-
-    return {bet: bet}
+    return {bet: 0.42 * table.players[table.activePlayer].stack}
 }
