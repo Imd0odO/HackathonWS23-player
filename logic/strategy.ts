d464882a2956cc9fd6570b610411c1ed1bd55469
Timo Schneider <timoschneider04@gmail.com>
2023-11-16 22:25:53 +0100

    mal logik?

--- logic/strategy.ts
+++ logic/strategy.ts
@@ -2,10 +2,29 @@ import {Bet, Table} from "../models";
 
 export function decide(table: Table): Bet {
     //TODO: Add Poker Logic Here... :)
-    let max_bet = 0;
-    table.players.forEach(player => {
-        max_bet = (player.bet > max_bet && player.stack != player.bet ? player.bet : max_bet);
-    })
+
+    let max_bet = table.minimumBet;
+
+    if (table.activePlayer != 0) {
+        let bets = table.players.map(player => player.bet)
+        bets.slice(table.activePlayer)
+
+        let normalized_sum = 0;
+
+        bets.forEach(bet => normalized_sum += Math.log(bet))
+
+        let normalized = normalized_sum / table.activePlayer
+
+        let highBet = false;
+        bets.forEach(bet => {
+            if (Math.log(bet) > 1.5*normalized) {
+                highBet = true
+            }
+        })
+        if (!highBet){
+            max_bet = table.players[table.activePlayer].stack
+        }
+    }
 
     return {bet: max_bet};
 }
