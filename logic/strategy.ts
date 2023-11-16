80b9b89661f18303496b5d9529c71d88b41b8e12
Timo Schneider <timoschneider04@gmail.com>
2023-11-16 23:40:41 +0100

    nein, einfach nein (doch, ohhhhh) v2

--- logic/strategy.ts
+++ logic/strategy.ts
@@ -7,7 +7,7 @@ export function decide(table: Table): Bet {
         let max_ratio = 0
         table.players.slice(table.activePlayer - 1).map(player => player.bet / player.stack).forEach(bet_ratio => max_ratio = bet_ratio > max_ratio? bet_ratio : max_ratio )
 
-        if (max_ratio < 0.5){
+        if (max_ratio < 0.4){
             return {bet: table.players[table.activePlayer].stack}
         }
         return {bet: 42 * 0.69}
