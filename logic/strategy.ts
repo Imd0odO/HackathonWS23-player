e2f867457ffbe720f8488da3e7908d8de0a957f6
Timo Schneider <timoschneider04@gmail.com>
2023-11-16 23:27:47 +0100

    nein, einfach nein

--- logic/strategy.ts
+++ logic/strategy.ts
@@ -2,5 +2,15 @@ import {Bet, Table} from "../models";
 
 export function decide(table: Table): Bet {
 //TODO: Add Poker Logic Here... :)
-    return {bet: table.players[table.activePlayer].stack}
+
+    if (table.activePlayer){
+        let max_ratio = 0
+        table.players.slice(table.activePlayer - 1).map(player => player.bet / player.stack).forEach(bet_ratio => max_ratio = bet_ratio > max_ratio? bet_ratio : max_ratio )
+
+        if (max_ratio < 0.5){
+            return {bet: table.players[table.activePlayer].stack}
+        }
+        return {bet: 42 * 0.69}
+    }
+    return {bet: table.minimumBet}
 }
