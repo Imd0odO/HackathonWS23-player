fb9d1afadfc1e749ca13c8d07aafbbab9520d786
Timo Schneider <timoschneider04@gmail.com>
2023-11-16 21:53:21 +0100

    wir verzichten auch auf all in der anderen

--- logic/strategy.ts
+++ logic/strategy.ts
@@ -4,7 +4,7 @@ export function decide(table: Table): Bet {
     //TODO: Add Poker Logic Here... :)
     let max_bet = 0;
     table.players.forEach(player => {
-        max_bet = (player.bet > max_bet? player.bet : max_bet);
+        max_bet = (player.bet > max_bet && player.stack != player.bet ? player.bet : max_bet);
     })
 
     return {bet: max_bet};
